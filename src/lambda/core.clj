dfeada48c3433a033ce07306761751172d57c226
ingun <ingun37@gmail.com>
2019-04-03 14:42:57 +0900

    AND

--- src/lambda/core.clj
+++ src/lambda/core.clj
@@ -4,7 +4,7 @@
 
 (defn incr [x] (+ x 1))
 (defn numcheck [n] ((n incr) 0))
-
+(defn boolcheck [b] (if (= ((b 0) 1) 0) true false))
 
 (def n0 (curry (fn [f x] x)))
 (def n1 (curry (fn [f x] (f x))))
@@ -13,10 +13,9 @@
 (def n4 (curry (fn [f x] (f (f (f (f x)))))))
 (def S (curry (fn [w y x] (y ((w y) x)))))
 (def M (curry (fn [x y z] (x (y z)))))
-;(defn M [x y] (fn [z] (x (y z))))
-;
-;(defn T [a b] a)
-;(defn F [a b] b)
+(def T (curry (fn [a b] a)))
+(def F (curry (fn [a b] b)))
+(def AND (curry (fn [x y] ((x y) F))))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
