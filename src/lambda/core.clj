f534ba554e5e8e928af292b6ffd9d8856208b65e
ingun <ingun37@gmail.com>
2019-04-03 14:33:15 +0900

    M

--- src/lambda/core.clj
+++ src/lambda/core.clj
@@ -1,16 +1,23 @@
 (ns lambda.core
   (:gen-class))
+(require '[uncomplicate.fluokitten.core :refer [curry]])
 
 (defn incr [x] (+ x 1))
-(defn numcheck [n] (n incr 0))
+(defn numcheck [n] ((n incr) 0))
 
-(defn n0 [f x] x)
-(defn n1 [f x] (f x))
-(defn n2 [f x] (f (f x)))
-(defn S [w] (fn [y x] (y (w y x))))
-(defn T [a b] a)
-(defn F [a b] b)
+
+(def n0 (curry (fn [f x] x)))
+(def n1 (curry (fn [f x] (f x))))
+(def n2 (curry (fn [f x] (f (f x)))))
+(def n3 (curry (fn [f x] (f (f (f x))))))
+(def n4 (curry (fn [f x] (f (f (f (f x)))))))
+(def S (curry (fn [w y x] (y (w y x)))))
+(def M (curry (fn [x y z] (x (y z)))))
+;(defn M [x y] (fn [z] (x (y z))))
+;
+;(defn T [a b] a)
+;(defn F [a b] b)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (numcheck (S n2))))
+  (println (numcheck n0)))
--- test/lambda/core_test.clj
+++ test/lambda/core_test.clj
@@ -1,7 +1,18 @@
 (ns lambda.core-test
+  (:use [clojure.repl :only (source)])
   (:require [clojure.test :refer :all]
             [lambda.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= (numcheck(S (S n2))) 4))))
+           (is (= (numcheck n0) 0))
+           (is (= (numcheck n1) 1))
+           (is (= (numcheck (S n3)) 4))
+           (is (= (numcheck (n4 S n2)) 6))
+;           (print (M n3 n2))
+           (is (= (numcheck (M n3 n2)) 6))
+;    (is (= (numcheck(S (S n2))) 4))
+;    (is (= (numcheck(n4 S n2)) 6))
+    ;(is (= (numcheck(M n3 n2)) 6))
+  )
+)
