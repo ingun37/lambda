e1e98d50269b40a23d67e5b34fab869bf4418fbe
ingun <ingun37@gmail.com>
2019-04-03 14:35:32 +0900

    curry

--- src/lambda/core.clj
+++ src/lambda/core.clj
@@ -11,7 +11,7 @@
 (def n2 (curry (fn [f x] (f (f x)))))
 (def n3 (curry (fn [f x] (f (f (f x))))))
 (def n4 (curry (fn [f x] (f (f (f (f x)))))))
-(def S (curry (fn [w y x] (y (w y x)))))
+(def S (curry (fn [w y x] (y ((w y) x)))))
 (def M (curry (fn [x y z] (x (y z)))))
 ;(defn M [x y] (fn [z] (x (y z))))
 ;
